1150eab789d3ee89f16dcf5d61d19c68a219d6b4
mixdup <jay.hunter@outlook.com.au>
2018-02-15 13:41:12 +1000

    commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, rtender_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return rtender_template(index.html)
+    return rtender_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
