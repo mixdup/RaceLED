6ae972d1582516ea585afe6d6c6228721c0c29b3
mixdup <jay.hunter@outlook.com.au>
2018-02-15 12:31:43 +1000

    commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+    return ('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
